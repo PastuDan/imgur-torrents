949f50f00e0f0364faf3bfe64134938658de10ba
Chase Hutchins <chase@imgur.com>
2017-03-01 19:49:24 -0800

    removing custom styling and adding some more pages

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,11 +7,66 @@ export default class Gallery extends Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-sm-4 col-md-15">
-          <Link to="/post/rAnDoM" className="thumbnail">
-            <img src="http://placehold.it/350x350" alt="img-block" />
-          </Link>
-        </div>
+          <div className="col-sm-4 col-md-3">
+            <Link to="/post/rAnDoM" className="thumbnail">
+              <img src="http://placehold.it/350x350" alt="img-block" />
+            </Link>
+          </div>
+          <div className="col-sm-4 col-md-3">
+            <Link to="/post/rAnDoM" className="thumbnail">
+              <img src="http://placehold.it/350x350" alt="img-block" />
+            </Link>
+          </div>
+          <div className="col-sm-4 col-md-3">
+            <Link to="/post/rAnDoM" className="thumbnail">
+              <img src="http://placehold.it/350x350" alt="img-block" />
+            </Link>
+          </div>
+          <div className="col-sm-4 col-md-3">
+            <Link to="/post/rAnDoM" className="thumbnail">
+              <img src="http://placehold.it/350x350" alt="img-block" />
+            </Link>
+          </div>
+          <div className="col-sm-4 col-md-3">
+            <Link to="/post/rAnDoM" className="thumbnail">
+              <img src="http://placehold.it/350x350" alt="img-block" />
+            </Link>
+          </div>
+          <div className="col-sm-4 col-md-3">
+            <Link to="/post/rAnDoM" className="thumbnail">
+              <img src="http://placehold.it/350x350" alt="img-block" />
+            </Link>
+          </div>
+          <div className="col-sm-4 col-md-3">
+            <Link to="/post/rAnDoM" className="thumbnail">
+              <img src="http://placehold.it/350x350" alt="img-block" />
+            </Link>
+          </div>
+          <div className="col-sm-4 col-md-3">
+            <Link to="/post/rAnDoM" className="thumbnail">
+              <img src="http://placehold.it/350x350" alt="img-block" />
+            </Link>
+          </div>
+          <div className="col-sm-4 col-md-3">
+            <Link to="/post/rAnDoM" className="thumbnail">
+              <img src="http://placehold.it/350x350" alt="img-block" />
+            </Link>
+          </div>
+          <div className="col-sm-4 col-md-3">
+            <Link to="/post/rAnDoM" className="thumbnail">
+              <img src="http://placehold.it/350x350" alt="img-block" />
+            </Link>
+          </div>
+          <div className="col-sm-4 col-md-3">
+            <Link to="/post/rAnDoM" className="thumbnail">
+              <img src="http://placehold.it/350x350" alt="img-block" />
+            </Link>
+          </div>
+          <div className="col-sm-4 col-md-3">
+            <Link to="/post/rAnDoM" className="thumbnail">
+              <img src="http://placehold.it/350x350" alt="img-block" />
+            </Link>
+          </div>
         </div>
       </div>
   )
--- src/components/Post.js
+++ src/components/Post.js
@@ -3,9 +3,26 @@ import React, { Component } from 'react';
 export default class Post extends Component {
   render() {
   	return (
-  	  <div className="container">
-  	    <p>Post!</p>
-  	  </div>
-	)
+      <div className="container">
+        <div className="row">
+          <div className="col-md-9">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                Post Title
+              </div>
+              <div className="panel-body">
+                Images...
+              </div>
+              <div className="panel-footer">
+                Post Meta
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            LE SIDEBAR
+          </div>
+        </div>
+      </div>
+    )
   }
 }
